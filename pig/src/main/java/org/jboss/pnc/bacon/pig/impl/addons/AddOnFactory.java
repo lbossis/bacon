baf1a8f7eabbecce2305248d93595131fa773aa2
Leonid Bossis <lbossis@redhat.com>
2022-04-05 22:24:37 -0400

    Initial version: implementing addon for scan service

--- pig/src/main/java/org/jboss/pnc/bacon/pig/impl/addons/AddOnFactory.java
+++ pig/src/main/java/org/jboss/pnc/bacon/pig/impl/addons/AddOnFactory.java
@@ -24,6 +24,7 @@ import org.jboss.pnc.bacon.pig.impl.addons.quarkus.QuarkusPostBuildAnalyzer;
 import org.jboss.pnc.bacon.pig.impl.addons.quarkus.VertxArtifactFinder;
 import org.jboss.pnc.bacon.pig.impl.addons.rhba.OfflineManifestGenerator;
 import org.jboss.pnc.bacon.pig.impl.addons.runtime.RuntimeDependenciesAnalyzer;
+import org.jboss.pnc.bacon.pig.impl.addons.scanservice.PostBuildScanService;
 import org.jboss.pnc.bacon.pig.impl.addons.spring.BomVerifierAddon;
 import org.jboss.pnc.bacon.pig.impl.addons.vertx.NotYetAlignedFromDependencyTree;
 import org.jboss.pnc.bacon.pig.impl.config.PigConfiguration;
@@ -41,11 +42,11 @@ import java.util.Map;
 public class AddOnFactory {
 
     public static List<AddOn> listAddOns(
-            PigConfiguration pigConfiguration,
-            Map<String, PncBuild> builds,
-            String releasePath,
-            String extrasPath,
-            Deliverables deliverables) {
+        PigConfiguration pigConfiguration,
+        Map<String, PncBuild> builds,
+        String releasePath,
+        String extrasPath,
+        Deliverables deliverables) {
         ArrayList<AddOn> resultList = new ArrayList<>();
 
         resultList.add(new RuntimeDependenciesAnalyzer(pigConfiguration, builds, releasePath, extrasPath));
@@ -54,11 +55,12 @@ public class AddOnFactory {
         resultList.add(new RuntimeDependenciesToAlignTree(pigConfiguration, builds, releasePath, extrasPath));
         resultList.add(new NotYetAlignedFromDependencyTree(pigConfiguration, builds, releasePath, extrasPath));
         resultList
-                .add(new QuarkusCommunityDepAnalyzer(pigConfiguration, builds, releasePath, extrasPath, deliverables));
+            .add(new QuarkusCommunityDepAnalyzer(pigConfiguration, builds, releasePath, extrasPath, deliverables));
         resultList.add(new MicroProfileSmallRyeCommunityDepAnalyzer(pigConfiguration, builds, releasePath, extrasPath));
         resultList.add(new QuarkusPostBuildAnalyzer(pigConfiguration, builds, releasePath, extrasPath));
         resultList.add(new OfflineManifestGenerator(pigConfiguration, builds, releasePath, extrasPath));
         resultList.add(new VertxArtifactFinder(pigConfiguration, builds, releasePath, extrasPath));
+        resultList.add(new PostBuildScanService(pigConfiguration, builds, releasePath, extrasPath));
         return resultList;
     }
 
---
+++ pig/src/main/java/org/jboss/pnc/bacon/pig/impl/addons/scanservice/PostBuildScanService.java
@@ -0,0 +1,35 @@
+package org.jboss.pnc.bacon.pig.impl.addons.scanservice;
+
+import org.jboss.pnc.bacon.pig.impl.addons.AddOn;
+import org.jboss.pnc.bacon.pig.impl.config.PigConfiguration;
+import org.jboss.pnc.bacon.pig.impl.pnc.PncBuild;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class PostBuildScanService extends AddOn {
+
+    public static final String NAME = "postBuildScanService";
+
+    private static final Logger log = LoggerFactory.getLogger(PostBuildScanService.class);
+
+    public PostBuildScanService(
+        PigConfiguration pigConfiguration,
+        Map<String, PncBuild> builds,
+        String releasePath,
+        String extrasPath) {
+        super(pigConfiguration, builds, releasePath, extrasPath);
+    }
+
+    @Override
+    protected String getName() {
+        return NAME;
+    }
+
+    @Override
+    public void trigger() {
+        String rs = " Post Build SCAN Service started";
+        log.info(rs);
+    }
+}
